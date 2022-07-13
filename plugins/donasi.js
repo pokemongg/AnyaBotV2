f97f9f55517b8f5f4c6d87fd7889550c8ef59a09
K0NC1T <68502508+koncit@users.noreply.github.com>
2022-07-13 14:41:15 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
-            .✵.𝗚𝗥𝗨𝗣 𝗣𝗘𝗡𝗧𝗔𝗕𝗢𝗧.✵.
+            .✵.*ANYA-BOT-V2*.✵.
 
-            https://chat.whatsapp.com/ElBB5NrJS637TYqjJLHuk2
+            *KAGA ADA :'*
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
