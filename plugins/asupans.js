239de52e7bc62da9e628305963d847731a672083
K0NC1T <68502508+koncit@users.noreply.github.com>
2022-07-14 13:21:15 +0700

    Update asupans.js

--- plugins/asupans.js
+++ plugins/asupans.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-	conn.sendFile(m.chat, 'https://api.zacros.my.id/asupan/random', 'asupan.mp4', '✿𝗖𝗥𝗘𝗔𝗧𝗢𝗥➢𝐆𝐞𝐦𝐩𝐲 𝐓𝐨𝐧\nSubscribe : https://www.youtube.com/channel/UCZoVmApPxtLYgUWwBD4nbCw', m)
+	conn.sendFile(m.chat, 'https://api.zacros.my.id/asupan/random', 'asupan.mp4', '✿𝗖𝗥𝗘𝗔𝗧𝗢𝗥➢*Koncit*\nSubscribe : https://www.youtube.com/channel/UClfUAjgtQ19vGnUbkQxTuAg/', m)
 }
 handler.help = ['asupan']
 handler.tags = ['asupan']
